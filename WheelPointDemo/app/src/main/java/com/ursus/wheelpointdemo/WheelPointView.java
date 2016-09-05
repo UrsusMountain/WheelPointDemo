ae07bc7edcf069178aae72e4cd372af76766bd49
ursus <1020220050@qq.com>
2016-09-05 16:14:40 +0800

    add WheelPointView

---
+++ WheelPointDemo/app/src/main/java/com/ursus/wheelpointdemo/WheelPointView.java
@@ -0,0 +1,25 @@
+package com.ursus.wheelpointdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Author:  ursus
+ * Date:    16/9/5
+ * Function:
+ */
+public class WheelPointView extends View{
+    
+    public WheelPointView(Context context) {
+        super(context);
+    }
+
+    public WheelPointView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WheelPointView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
