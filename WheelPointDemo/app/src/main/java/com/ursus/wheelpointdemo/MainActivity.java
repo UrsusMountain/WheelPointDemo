475c8fd9b76580bd12212e31ac905acd7fe787c7
ursus <1020220050@qq.com>
2016-09-06 15:36:15 +0800

    The function of simple

--- WheelPointDemo/app/src/main/java/com/ursus/wheelpointdemo/MainActivity.java
+++ WheelPointDemo/app/src/main/java/com/ursus/wheelpointdemo/MainActivity.java
@@ -1,13 +1,72 @@
 package com.ursus.wheelpointdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.viewPager)
+    ViewPager viewPager;
+    @Bind(R.id.wheelpointview)
+    WheelPointView wheelpointview;
+    private List<View> viewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        viewList = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            View view = getLayoutInflater().inflate(R.layout.layout_demo, null);
+            viewList.add(view);
+        }
+        viewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return viewList.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                container.addView(viewList.get(position));
+                return viewList.get(position);
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(viewList.get(position));
+            }
+        });
+        wheelpointview.bindViewPager(viewPager);
+    }
+
+    @OnClick(R.id.btn_change)
+    public void onClick() {
+        switch (wheelpointview.getMode()) {
+            case WheelPointView.SCROLL_MODE_NORMAL:
+                wheelpointview.setMode(WheelPointView.SCROLL_MODE_VISCOSITY);
+                break;
+            case WheelPointView.SCROLL_MODE_VISCOSITY:
+                wheelpointview.setMode(WheelPointView.SCROLL_MODE_NORMAL);
+                break;
+        }
     }
 }
--- WheelPointDemo/app/src/main/java/com/ursus/wheelpointdemo/WheelPointView.java
+++ WheelPointDemo/app/src/main/java/com/ursus/wheelpointdemo/WheelPointView.java
@@ -1,25 +1,153 @@
 package com.ursus.wheelpointdemo;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.FloatRange;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
-/**
- * Author:  ursus
- * Date:    16/9/5
- * Function:
- */
-public class WheelPointView extends View{
-    
+public class WheelPointView extends View {
+
+    public final static int SCROLL_MODE_NORMAL = 0;
+    public final static int SCROLL_MODE_VISCOSITY = 1;
+
+    private int mWidth;
+    private int mHeight;
+    private int mPointRadius;
+    private int mPointGap;
+    private int mPointCount;
+    private int mRealWidth;
+
+    private int mSelectedPoint = 0;
+    private float mMoveScale = 0;
+
+    private Paint mPaint;
+
+    private int mMode;
+
     public WheelPointView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public WheelPointView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public WheelPointView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mPointCount = 5;
+        mMode = SCROLL_MODE_NORMAL;
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        this.mWidth = w;
+        this.mHeight = h;
+        mPointGap = Math.min(mWidth / (mPointCount * 2), mHeight);
+        mPointRadius = mPointGap / 2;
+        mRealWidth = mPointCount * (mPointRadius * 2 + mPointGap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        initPaint();
+        canvas.translate(0, mHeight / 2);
+
+        for (int i = 0; i < mPointCount; i++) {
+            float x = (float) ((2 * mPointRadius + mPointGap) * (i + 0.5));
+            canvas.drawCircle(x, 0, mPointRadius, mPaint);
+        }
+
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        float xPos = (float) ((2 * mPointRadius + mPointGap) * (mSelectedPoint + 0.5 + mMoveScale));
+        if (xPos > mRealWidth) {
+            xPos = xPos - mRealWidth;
+        }
+
+        switch (mMode) {
+            case SCROLL_MODE_NORMAL:
+                drawNormal(canvas,xPos);
+                break;
+            case SCROLL_MODE_VISCOSITY:
+                drawViscosity(canvas, xPos);
+                break;
+        }
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.GRAY);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(4);
+    }
+
+    private void drawNormal(Canvas canvas, float x) {
+        canvas.drawCircle(x, 0, mPointRadius, mPaint);
+    }
+
+    private void drawViscosity(Canvas canvas, float x) {
+        float fx = (float) (4 * Math.pow(mMoveScale, 2) - 4 * mMoveScale + 1);
+        canvas.drawCircle(x, 0, mPointRadius * (fx), mPaint);
+        Path path = new Path();
+        float startX = (float) ((2 * mPointRadius + mPointGap) * (mSelectedPoint + 0.5));
+        float startY = (float) (-mPointRadius * (1 - (mMoveScale * 0.9)));
+        float endX = (float) ((2 * mPointRadius + mPointGap) * (mSelectedPoint + 0.5 + mMoveScale));
+        float endY = -mPointRadius * (fx);
+        float controlX = (startX + endX) / 2;
+        float controlY = 0;
+        path.moveTo(startX, startY);
+        path.quadTo(controlX, controlY, endX, endY);
+        path.lineTo(endX, -endY);
+        path.quadTo(controlX, controlY, startX, -startY);
+        path.close();
+        canvas.drawCircle(startX, 0, (float) (mPointRadius * (1 - (mMoveScale * 0.9))), mPaint);
+        canvas.drawPath(path, mPaint);
+    }
+
+    protected void setMoveScale(@FloatRange(from = 0.0, to = 1.0) float scale, int position) {
+        mMoveScale = scale;
+        mSelectedPoint = position;
+        invalidate();
+    }
+
+    public void setMode(int mMode) {
+        this.mMode = mMode;
+    }
+
+    public int getMode(){
+        return mMode;
+    }
+
+    public void bindViewPager(ViewPager viewPager) {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.i("Point", position + "__" + positionOffset + "__" + positionOffsetPixels);
+                int selectedPosition = position;
+                if (position > mPointCount - 1) {
+                    selectedPosition = position % mPointCount;
+                }
+                setMoveScale(positionOffset, selectedPosition);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
